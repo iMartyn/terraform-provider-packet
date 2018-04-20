a2535b0afef97ea4d4451270955abfba7eae9135
Martyn Ranyard <martyn.ranyard@pubnative.net>
2018-04-20 18:31:08 +0200

    Adding the basics

--- packet/resource_packet_project.go
+++ packet/resource_packet_project.go
@@ -43,23 +43,39 @@ func resourcePacketProject() *schema.Resource {
 				},
 				ValidateFunc: validation.StringMatch(uuidRE, "must be a valid UUID"),
 			},
+			"organization_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
+					return strings.ToLower(strings.Trim(old, `"`)) == strings.ToLower(strings.Trim(new, `"`))
+				},
+				ValidateFunc: validation.StringMatch(uuidRE, "must be a valid UUID"),
+			},
 		},
 	}
 }
 
 func resourcePacketProjectCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*packngo.Client)
-
-	createRequest := &packngo.ProjectCreateRequest{
-		Name: d.Get("name").(string),
+	if d.Get("organization_id").(string) != "" {
+		createRequest := &packngo.OrganizationProjectCreateRequest{
+			Name: d.Get("name").(string),
+		}
+		project, _, err := client.Organizations.CreateOrganizationProject(d.Get("organization_id").(string), createRequest)
+		if err != nil {
+			return friendlyError(err)
+		}
+		d.SetId(project.ID)
+	} else {
+		createRequest := &packngo.ProjectCreateRequest{
+			Name: d.Get("name").(string),
+		}
+		project, _, err := client.Projects.Create(createRequest)
+		if err != nil {
+			return friendlyError(err)
+		}
+		d.SetId(project.ID)
 	}
-
-	project, _, err := client.Projects.Create(createRequest)
-	if err != nil {
-		return friendlyError(err)
-	}
-
-	d.SetId(project.ID)
 
 	return resourcePacketProjectRead(d, meta)
 }
@@ -86,6 +102,7 @@ func resourcePacketProjectRead(d *schema.ResourceData, meta interface{}) error {
 	d.Set("name", proj.Name)
 	d.Set("created", proj.Created)
 	d.Set("updated", proj.Updated)
+	d.Set("organization_id", proj.Organization)
 
 	return nil
 }
